0b38d56fd734a890c0a63e9cc50898825608bfaa
Mohammed-Rifaque <cmohammedrifaque22@gmail.com>
2025-01-15 00:00:43 +0530

    fixes

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,7 +4,6 @@ import React from "react";
 const Main = () => {
   return (
     <div className="container mx-auto px-4">
-      <h1 className="text-2xl font-bold mb-4">Task Manager</h1>
 
     </div>
   );
